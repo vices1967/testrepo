5f8a0ca8b90577c7e04531b0d1944fe8471d197b
vices1967 <83917324+vices1967@users.noreply.github.com>
2024-05-28 00:42:45 -0300

    Update firstpython.py
    comentario para pylint

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,7 @@
-# Display de output
+"""
+Este módulo contiene ejemplos de cómo usar Pylint
+y resolver problemas comunes de estilo de código.
+"""
+
+print("Hola, mundo!")
 print("first python file")
